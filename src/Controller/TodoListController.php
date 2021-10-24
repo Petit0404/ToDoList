e79ab0828a238308d1fc15ce53493b75ddc3ba9b
Marion Kann <marionkannengiesser@gmail.com>
2021-10-24 09:20:36 +0200

    ajout des listes et tâches

---
+++ src/Controller/TodoListController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\TodoList;
+use App\Form\TodoListType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TodoListController extends AbstractController {
+    /**
+     * @Route("/create-list", name="create_list")
+     */
+    public function create(Request $request): Response {
+        $todoList = new TodoList();
+
+        $form = $this->createForm(TodoListType::class, $todoList);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($todoList);
+            $em->flush();
+            return $this->redirectToRoute("read_all");
+        }
+
+        return $this->render("todo_list/create.html.twig", [
+            "form" => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/", name="read_all")
+     */
+    public function readAll(): Response {
+        $repo = $this->getDoctrine()->getRepository(TodoList::class);
+        $lists = $repo->findAll();
+
+        return $this->render("todo_list/index.html.twig", [
+            "lists" => $lists
+        ]);
+    }
+
+    /**
+     * @Route("/update-list/{id}", name="update_list")
+     */
+    public function update(TodoList $list, Request $request): Response {
+        $form = $this->createForm(TodoListType::class, $list);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+            return $this->redirectToRoute("read_all");
+        }
+
+        return $this->render("todo_list/create.html.twig", [
+            "form" => $form->createView(),
+            "list" => $list
+        ]);
+    }
+
+    /**
+     * @Route("/delete-list/{id}", name="delete_list")
+     */
+    public function delete(TodoList $list): Response {
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($list);
+        $em->flush();
+        return $this->redirectToRoute("read_all");
+    }
+}
--- src/Entity/ToDoList.php
+++ src/Entity/ToDoList.php
@@ -1,54 +1,54 @@
 <?php
 
-namespace App\Entity; 
+namespace App\Entity;
 
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
-
 /**
  * @ORM\Entity
  * @ORM\Table(name="list")
  */
-
-class ToDoList {
+class TodoList {
     /**
      * @ORM\Id
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
      */
     private $id;
+
     /**
-     * @Assert\NotBlank(message="veuillez renseigner un nom")
+     * @Assert\NotBlank(message="Veuillez renseigner le nom de la liste")
      * @Assert\Length(
-     *  min=2,
-     *  max=80,
-     *  minMessage="le nom est trop court",
-     *  maxMessage="le nom est trop long"
+     *      min=2,
+     *      max=80,
+     *      minMessage="Le nom saisi est trop court",
+     *      maxMessage="Le nom saisi est trop long"
      * )
      * @ORM\Column(type="string", length=80)
      */
     private $name;
+
     /**
-     * @Assert\NotBlank(message="veuillez renseigner une couleur")
+     * @Assert\NotBlank(message="Veuillez renseigner la couleur de la liste")
      * @Assert\Length(
-     * min=2,
-     * max=10,
-     * minMessage="le couleur est trop courte",
-     * maxMessage="le couleur est trop longue"
+     *      min=2,
+     *      max=10,
+     *      minMessage="La couleur saisie est trop courte",
+     *      maxMessage="La couleur saisie est trop longue"
      * )
      * @ORM\Column(type="string", length=10)
      */
-    private $color; 
+    private $color;
 
     /**
      * @ORM\OneToMany(targetEntity=Task::class, mappedBy="list", orphanRemoval=true)
      */
     private $tasks;
 
-    public function __construct(){
+    public function __construct() {
         $this->tasks = new ArrayCollection();
     }
 
@@ -108,6 +108,36 @@ class ToDoList {
     public function setColor($color)
     {
         $this->color = $color;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Task[]
+     */
+    public function getTasks(): Collection
+    {
+        return $this->tasks;
+    }
+
+    public function addTask(Task $task): self
+    {
+        if (!$this->tasks->contains($task)) {
+            $this->tasks[] = $task;
+            $task->setList($this);
+        }
+
+        return $this;
+    }
+
+    public function removeTask(Task $task): self
+    {
+        if ($this->tasks->removeElement($task)) {
+            // set the owning side to null (unless already changed)
+            if ($task->getList() === $this) {
+                $task->setList(null);
+            }
+        }
 
         return $this;
     }
